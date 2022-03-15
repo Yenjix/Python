3ca367b5ddfe1e30f1e8fd0d75da06e692e89ed1
Pedro Henrique Anjos <94488649+Yenjix@users.noreply.github.com>
2022-03-15 20:42:38 -0300

    Update __init__.py
    Adicionando informações: como dar upload no package para a comunidade do Python

--- Package-Exemple-Cars/__init__.py
+++ Package-Exemple-Cars/__init__.py
@@ -1,5 +1,7 @@
 # Exemplo tirado de: https://www.geeksforgeeks.org/create-access-python-package/#:~:text=First%2C%20we%20create%20a%20directory,the%20directory%20is%20a%20package.
 # 15/03/2022 20h39
+# Ver também: https://docs.google.com/presentation/d/1gzBKKZdtJdDfhKBF_0Yr4k6Xj8yke2CT/edit#slide=id.p32
+# lá tem instruções de como upar para a comunidade do Python como um todo
 from Bmw import Bmw
 from Audi import Audi
 from Nissan import Nissan
